217022b435564d5fd3f3a41b4997e7e220f6d2e7
dheeraj <dheerajdataclaps@gmail.com>
2025-07-27 18:03:08 +0530

    url changes

--- to_do_app/urls.py
+++ to_do_app/urls.py
@@ -27,6 +27,6 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('accounts/', include('accounts.urls')),
-    path('firm/', include('firm.urls')),
+    path('api/accounts/', include('accounts.urls')),
+    path('api/firm/', include('firm.urls')),
 ]
